5c2e06ef7a1095001fcf031769e3b9fb8ef2fdd8
ethadnguyen <tringuyen1382003@gmail.com>
2024-09-23 16:33:56 +0700

    fix api

--- server/index.js
+++ server/index.js
@@ -10,11 +10,7 @@ const app = express();
 
 dotenv.config();
 app.use(express.json());
-app.use(cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
-}));
+app.use(cors());
 
 app.use('/api/users', userRoutes);
 app.use('/api/chats', chatRoutes);
